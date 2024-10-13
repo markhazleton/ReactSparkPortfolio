1df00ddb4166a58acbb938afc5f11a5db921546c
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-10-13 10:06:09 -0500

    remove .js files change config to not emit, add joke api call

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   build: {
     outDir: "docs",
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',  // Alias for react-native-web
+    },
   },
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/rss-feed": {
-        target: "https://markhazleton.com", // Target RSS feed domain
+        target: "https://markhazleton.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/rss-feed/, "/rss.xml"), // Rewrite to the RSS path
-        secure: true, // Set to true if you are using https
+        rewrite: (path) => path.replace(/^\/rss-feed/, "/rss.xml"),
+        secure: true,
       },
     },
+  },
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
   },
 });

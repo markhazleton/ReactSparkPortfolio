50e78c1f463f72a604904acb98bf3c165c791c19
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-10-14 13:17:25 -0500

    fix prod env on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import * as dotenv from "dotenv";
+
+// Load environment variables based on the current NODE_ENV or default to `.env`
+const envFileName = `.env.${process.env.NODE_ENV || "development"}`;
+dotenv.config({ path: envFileName });
 
 // Get the current date-time during the build
 const buildDate = new Date().toISOString();
 
-
+console.log("VITE_BASE_URL:", process.env.VITE_BASE_URL); // Add this to debug
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || "/",
   build: {
     outDir: "docs",
   },
@@ -17,7 +22,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'react-native': 'react-native-web',  // Alias for react-native-web
+      "react-native": "react-native-web", // Alias for react-native-web
     },
   },
   plugins: [react()],
@@ -33,7 +38,7 @@ export default defineConfig({
     },
   },
   esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
   },
 });

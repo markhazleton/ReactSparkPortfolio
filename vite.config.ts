02fc02d74c79b4430e25a1df3c1aa482721f12c8
Mark Hazleton <mark.hazleton@controlorigins.com>
2025-04-14 08:01:21 -0500

    updates for doc folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import * as dotenv from "dotenv";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import fs from "fs";
 
 // Load environment variables based on the current NODE_ENV or default to `.env`
 const envFileName = `.env.${process.env.NODE_ENV || "development"}`;
@@ -12,6 +13,16 @@ dotenv.config({ path: envFileName });
 const buildDate = new Date().toISOString();
 
 console.log("VITE_BASE_URL:", process.env.VITE_BASE_URL); // Add this to debug
+
+// Function to create .nojekyll file
+const createNoJekyllFile = () => {
+  return {
+    name: "create-nojekyll-file",
+    closeBundle: () => {
+      fs.writeFileSync("docs/.nojekyll", "");
+    },
+  };
+};
 
 export default defineConfig({
   base: process.env.VITE_BASE_URL || "/",
@@ -29,6 +40,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    createNoJekyllFile(),
     viteStaticCopy({
       targets: [
         {
